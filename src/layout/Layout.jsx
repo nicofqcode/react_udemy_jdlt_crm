c98254fe8c1abdb32cddb2533206e08cc46c53f0
Nicolás F <83544753+nicofqcode@users.noreply.github.com>
2022-06-09 16:46:04 -0400

    first commit

---
+++ src/layout/Layout.jsx
@@ -0,0 +1,35 @@
+import { Outlet, Link, useLocation } from 'react-router-dom'
+
+
+const Layaout = () => {
+
+  const location = useLocation()
+
+  const urlActual = location.pathname
+
+  return (
+    <div className="md:flex md:min-h-screen">
+
+      <div className="md:w-1/4 bg-blue-900 px-5 py-10">
+        <h2 className='text-4xl font-black text-center text-white'>CRM - Clientes</h2>
+      
+        <nav className="mt-10">
+          <Link 
+            className={`${urlActual === '/clientes' ? 'text-blue-300' : 'text-white'} text-2xl block mt-2 hover:text-blue-300`} 
+            to="/clientes"
+          >Clientes</Link>
+          <Link 
+            className={`${urlActual === '/clientes/nuevo' ? 'text-blue-300' : 'text-white'} text-2xl block mt-2 hover:text-blue-300`} 
+            to="/clientes/nuevo"
+          >Nuevo Cliente</Link>
+        </nav>
+      </div>
+
+      <div className="contenedor-table md:w-3/4  md:h-screen md:overflow-scroll">
+        <Outlet />
+      </div>
+    </div>
+  )
+}
+
+export default Layaout
